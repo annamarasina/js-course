e05210b51dc664eacb5fa409b788d6625084cc14
annamarasina <159254541+annamarasina@users.noreply.github.com>
2024-02-09 00:47:19 +0200

    followed along, practical task of making five parameters, adding them together,  setting a variable, and console logging it didnt work

--- course.js
+++ course.js
@@ -13,3 +13,23 @@ const five=5;
 console.log(one+two+three+four+five);
 const sum = one+two+three+four+five;
 console.log(sum);
+
+const sumTwoNumbers = (firstNumber, secondNumber) => {
+    return firstNumber + secondNumber;
+};
+
+const result = sumTwoNumbers(1,2);
+const secondResult = sumTwoNumbers(5, 4);
+console.log(result, secondResult);
+
+const sumFiveParameters = (firstPar, secondPar, thirdPar, fourthPar, fifthPar) => {
+    return firstPar + secondPar + thirdPar + fourthPar + fifthPar;
+};
+const resultPar = sumFiveParameters(1,2,3,4,5);
+console.log(resultPar);
+
+const sumFiveNumbers = (firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber) => {
+    return firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber;
+    };
+const resultFiveNumbers = sumFiveNumbers(1,2,3,4,5);
+console.log(resultNumbers);
