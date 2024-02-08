8ad3d25e1c04dcee52cbdff5e3a8d1b905857d7f
annamarasina <159254541+annamarasina@users.noreply.github.com>
2024-02-09 00:24:15 +0200

    created html and js files
    tried out what he showed in the video and did his first practical task (created five different variables with different numbers and added them together).

---
+++ course.js
@@ -0,0 +1,15 @@
+console.log(1+1);
+const first = 1;
+let second = first+first;
+
+second = 3;
+console.log(first + second);
+
+const one = 1;
+const two = 2;
+const three = 3;
+const four =4;
+const five=5;
+console.log(one+two+three+four+five);
+const sum = one+two+three+four+five;
+console.log(sum);
