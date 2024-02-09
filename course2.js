5f9876182eb0d7cb690dad84b10fea127fecf418
annamarasina <159254541+annamarasina@users.noreply.github.com>
2024-02-10 01:41:07 +0200

    for and while loops tasks

--- course2.js
+++ course2.js
@@ -18,7 +18,7 @@ const user = {
 
 const roleReturn = (user) => {
     if (user.role === undefined) {
-        throw new Error("role is not provided");
+ //       throw new Error("role is not provided");
     }
     return user.role;
 };
@@ -26,3 +26,43 @@ const roleReturn = (user) => {
 const result = roleReturn(user);
 
 console.log(result);
+
+
+//for loops
+const numbers = [];
+for (let i = 0; i < 5; i++) {
+    numbers.push(i);
+}
+console.log(numbers);
+
+//task
+const emptyArray = [];
+for (let i=0;i<10;i++) {
+    emptyArray.push({
+        id: i,
+        name: "hello_" + i,
+    });
+};
+console.log(emptyArray);
+
+
+
+
+//while loop
+let i =0;
+while (i<10) {
+    console.log(i);
+    i +=2;
+};
+
+//task
+const emptyWhileArray = [];
+while (i<10) {
+    emptyWhileArray.push({
+        id: i,
+        name: "hello_" + i,
+        
+    });
+    i += 3;
+};
+console.log(emptyWhileArray);
